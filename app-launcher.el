7ead23d5830acb939c3eb5a61bb49f0dfcdcba2e
Sergey Trofimov <sarg@sarg.org.ru>
2025-04-09 14:16:21 +0200

    Support absolute path in Icon

--- app-launcher.el
+++ app-launcher.el
@@ -98,7 +98,9 @@
 
 (defun app-launcher--get-icon (path icon)
   "Find the requested ICON in the requested PATH."
-  (when-let* ((icon-file (locate-file icon path app-launcher-icon-extensions)))
+  (when-let* ((icon-file (if (and (file-name-absolute-p icon) (file-exists-p icon))
+                             icon
+                           (locate-file icon path app-launcher-icon-extensions))))
     (create-image icon-file nil nil :ascent 'center
                   :scale 1.0
                   :height '(1.0 . ch))))
